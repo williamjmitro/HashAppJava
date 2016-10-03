225b137904b80d7737e1464deb8b42861529e5d6
Bill <williajmitro@gmail.com>
2016-10-03 18:42:34 -0400

    Rest stuff. Probably doesnt work

---
+++ src/main/java/com/api/endpoint/HasherEndpoint.java
@@ -0,0 +1,7 @@
+package com.api.endpoint;
+
+/**
+ * Created by Porn Again Christian PRATH3 on 10/3/2016.
+ */
+public class HasherEndpoint {
+}
---
+++ src/main/java/com/api/service/HasherService.java
@@ -0,0 +1,7 @@
+package com.api.service;
+
+/**
+ * Created by Porn Again Christian PRATH3 on 10/3/2016.
+ */
+public interface HasherService {
+}
