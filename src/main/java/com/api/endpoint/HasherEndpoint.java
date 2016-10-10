8cf4c0e29b92d6d3bd60d198035ad42fc0f2afaf
Bill <williajmitro@gmail.com>
2016-10-10 13:38:35 -0400

    CORS Filter

--- src/main/java/com/api/endpoint/HasherEndpoint.java
+++ src/main/java/com/api/endpoint/HasherEndpoint.java
@@ -6,6 +6,7 @@ import com.api.service.HasherService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,7 @@ public class HasherEndpoint extends BaseEndpoint {
     @Autowired
     HasherService hasherService;
 
+    @CrossOrigin
     @RequestMapping(value = "/all",method = RequestMethod.GET,produces = "application/json")
     @ApiOperation(value = "Returns all hashers")
     public List<Hasher> getHashers(){
